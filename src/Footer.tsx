64c4e45e99316eb883f85d1fabf154dbbb817531
Oscar Osses <oscarossesa@gmail.com>
2023-04-30 21:08:42 -0400

    adding reset game and devtools

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,11 +1,21 @@
+import { Button } from "@mui/material"
 import { useQuestionData } from "./hooks/useQuestionData"
+import { useQuestionStore } from "./store/questions"
 
 export const Footer = () => {
+  const reset = useQuestionStore(store => store.reset)
   const { correct, incorrect, unanswer } = useQuestionData()
+
+  const handleReset = () => reset()
 
   return (
     <footer style={{ marginTop: '16px' }}>
       <strong>{`${correct} correctas - ${incorrect} incorrectas - ${unanswer}`}</strong>
+      <div>
+        <Button onClick={handleReset}>
+          Reset Game
+        </Button>
+      </div>
     </footer>
   )
 }
--- src/store/questions.ts
+++ src/store/questions.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
+import { devtools, persist } from 'zustand/middleware'
 import { type Question } from '../types'
 import confetti from 'canvas-confetti'
 
@@ -10,58 +10,64 @@ interface State {
   selectAnswer: (questionId: number, answerIndex: number) => void,
   goNextQuestion: () => void,
   goPreviousQuestion: () => void,
+  reset: () => void,
 }
 
 export const useQuestionStore = create<State>()(
-  persist(
-    (set, get) => ({    
-      questions: [],
-      currentQuestion: 0,
-      fetchQuestions: async (limit: number) => {
-        const res = await fetch('http://localhost:5173/data.json')
-        const json = await res.json()
+  devtools(
+    persist(
+      (set, get) => ({    
+        questions: [],
+        currentQuestion: 0,
+        fetchQuestions: async (limit: number) => {
+          const res = await fetch('http://localhost:5173/data.json')
+          const json = await res.json()
 
-        const questions = json.sort(() => Math.random() - 0.5).slice(0, limit)
+          const questions = json.sort(() => Math.random() - 0.5).slice(0, limit)
 
-        set({ questions })
-      },
-      selectAnswer: (questionId: number, answerIndex: number) => {
-        const { questions } = get()
+          set({ questions }, false, 'FETCH_QUESTIONS')
+        },
+        selectAnswer: (questionId: number, answerIndex: number) => {
+          const { questions } = get()
 
-        const newQuestions = structuredClone(questions)
-        const questionIndex = questions.findIndex(q => q.id === questionId)
-        const questionInfo = newQuestions[questionIndex]
-        const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex
-        
-        if (isCorrectUserAnswer) confetti()
+          const newQuestions = structuredClone(questions)
+          const questionIndex = questions.findIndex(q => q.id === questionId)
+          const questionInfo = newQuestions[questionIndex]
+          const isCorrectUserAnswer = questionInfo.correctAnswer === answerIndex
+          
+          if (isCorrectUserAnswer) confetti()
 
-        newQuestions[questionIndex] = {
-          ...questionInfo,
-          isCorrectUserAnswer,
-          userSelectedAnswer: answerIndex
+          newQuestions[questionIndex] = {
+            ...questionInfo,
+            isCorrectUserAnswer,
+            userSelectedAnswer: answerIndex
+          }
+          
+          set({ questions: newQuestions }, false, 'SELECT_ANSWER')
+        },
+        goNextQuestion: () => {
+          const { currentQuestion, questions } = get()
+          const nextQuestion = currentQuestion + 1
+
+          if (nextQuestion < questions. length) {
+            set({ currentQuestion: nextQuestion}, false, 'GO_NEXT_QUESTION')
+          }
+        },
+        goPreviousQuestion: () => {
+          const { currentQuestion } = get()
+          const previousQuestion = currentQuestion - 1
+
+          if (previousQuestion >= 0) {
+            set({ currentQuestion: previousQuestion}, false, 'GO_PREVIOUS_QUESTION')
+          }
+        },
+        reset: () => {
+          set({ currentQuestion: 0, questions: [] }, false, 'RESET')
         }
-        
-        set({ questions: newQuestions })
-      },
-      goNextQuestion: () => {
-        const { currentQuestion, questions } = get()
-        const nextQuestion = currentQuestion + 1
-
-        if (nextQuestion < questions. length) {
-          set({ currentQuestion: nextQuestion})
-        }
-      },
-      goPreviousQuestion: () => {
-        const { currentQuestion } = get()
-        const previousQuestion = currentQuestion - 1
-
-        if (previousQuestion >= 0) {
-          set({ currentQuestion: previousQuestion})
-        }
+      }),
+      {
+        name: 'question-store',
       }
-    }),
-    {
-      name: 'question-store',
-    }
+    )
   )
 )
